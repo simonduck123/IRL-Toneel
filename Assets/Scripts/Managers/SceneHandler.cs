9d7e54cd92693e26b5f594b5fd8d818b8abfe757
simonduck123 <la19039906@gmail.com>
2025-10-24 11:03:39 +0200

    Transitions + Example Scene

--- Assets/Scripts/Managers/SceneHandler.cs
+++ Assets/Scripts/Managers/SceneHandler.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using System.Collections;
@@ -9,5 +10,73 @@ public class SceneHandler : MonoBehaviour
     {
         SceneManager.LoadScene((int)sceneIndex);
         Debug.Log("Scene Loaded: " + (int)sceneIndex);
+    }
+
+    public void SceneLoaderAsync(float sceneIndex)
+    {
+        StartCoroutine(LoadSceneAsync(sceneIndex));
+    }
+
+    private IEnumerator LoadSceneAsync(float sceneIndex)
+    {
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync((int)sceneIndex);
+        asyncLoad.allowSceneActivation = false;
+
+        while (!asyncLoad.isDone)
+        {
+            Debug.Log($"Loading progress: {asyncLoad.progress * 100}%");
+
+            if (asyncLoad.progress >= 0.9f)
+            {
+                Debug.Log("Scene is ready, activating...");
+                asyncLoad.allowSceneActivation = true;
+            }
+
+            yield return null;
+        }
+    }
+
+    // 🆕 Additive load version
+    public void LoadSceneAdditive(float sceneIndex)
+    {
+        StartCoroutine(LoadSceneAdditiveAsync(sceneIndex));
+    }
+
+    private IEnumerator LoadSceneAdditiveAsync(float sceneIndex)
+    {
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync((int)sceneIndex, LoadSceneMode.Additive);
+        asyncLoad.allowSceneActivation = false;
+
+        while (!asyncLoad.isDone)
+        {
+            Debug.Log($"[Additive] Loading progress: {asyncLoad.progress * 100}%");
+
+            if (asyncLoad.progress >= 0.9f)
+            {
+                Debug.Log("[Additive] Scene is ready, activating...");
+                asyncLoad.allowSceneActivation = true;
+            }
+
+            yield return null;
+        }
+
+        Debug.Log("[Additive] Scene loaded additively!");
+    }
+
+    // 🧹 Optional: Unload additive scenes by index
+    public void UnloadScene(float sceneIndex)
+    {
+        SceneManager.UnloadSceneAsync((int)sceneIndex);
+        Debug.Log("Scene Unloaded: " + (int)sceneIndex);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha0)) SceneManager.LoadScene(0);
+        else if (Input.GetKeyDown(KeyCode.Alpha1)) SceneManager.LoadScene(1);
+        else if (Input.GetKeyDown(KeyCode.Alpha2)) SceneManager.LoadScene(2);
+        else if (Input.GetKeyDown(KeyCode.Alpha3)) SceneManager.LoadScene(3);
+        else if (Input.GetKeyDown(KeyCode.Alpha4)) SceneManager.LoadScene(4);
+        else if (Input.GetKeyDown(KeyCode.Alpha5)) SceneManager.LoadScene(5);
     }
 }
