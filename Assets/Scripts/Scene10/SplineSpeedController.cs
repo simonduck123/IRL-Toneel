04172ad8463792b30fefac9f7e3c7bad3efa9d04
Simon Duckham <la19039906@gmail.com>
2026-02-06 12:02:01 +0100

    scene 10 changes

--- Assets/Scripts/Scene10/SplineSpeedController.cs
+++ Assets/Scripts/Scene10/SplineSpeedController.cs
@@ -4,13 +4,12 @@ using UnityEngine.Splines;
 
 public class SplineSpeedController : MonoBehaviour
 {
-    [SerializeField] private float dollySpeed = 0.015f;
     public CinemachineSplineDolly cinemachineSplineDolly;
     
-    public void SetSpeed()
+    public void SetSpeed(float speed)
     {
         var autodolly = cinemachineSplineDolly.AutomaticDolly.Method as SplineAutoDolly.FixedSpeed;
         if (autodolly != null)
-            autodolly.Speed = dollySpeed;
+            autodolly.Speed = speed;
     }
 }
